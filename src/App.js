1775e73d037f9d22df636c2a85ba1cdc97745989
Pablo Velez <pvelez@bp-3.com>
2019-05-05 20:20:57 -0500

    set the app standard

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
-import UserInput from './components/UserInput';
-import UserOutput from './components/UserOutput';
 import './App.css';
 
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "Angelica",
-      lastname: "Cuellar"
-    }
-    this.changeUser = this.changeUser.bind(this);
-  }
-
-  changeUser(e) {
-    this.setState({username: e.target.value});
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <UserInput changeUser={this.changeUser} username={this.state.username}></UserInput>
-        <UserOutput username={this.state.username} lastname={this.state.lastname}></UserOutput>
-      </div>
-    );
+class App extends Component{
+  render = () => { 
+    return (<div className="App">
+      <h1>Esta es la app</h1>
+    </div>)
   }
 }
 
