a6f854268c54dcf3e72fb39cc130dc2f29b48d11
Pablo Velez <pvelez@bp-3.com>
2020-09-29 14:14:58 +0200

    restructure code and add the logger middleware

--- redux--03-react-redux/src/index.js
+++ redux--03-react-redux/src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log(
+        `Action: ${action.type} ** Middleware before: `,
+        store.getState()
+      );
+      const result = next(action);
+      console.log("** Middelware after", store.getState());
+      return result;
+    };
+  };
+};
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
